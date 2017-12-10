beb4ae0d7affcf71cf1a5d14441dd8d8e9f61a12
sans2010 <sans2010@gmail.com>
2017-12-10 15:48:46 +0530

    new class added

--- src/test/java/com/qaagility/controller/CalcTest.java
+++ src/test/java/com/qaagility/controller/CalcTest.java
@@ -5,9 +5,15 @@ import static org.junit.Assert.assertEquals;
 
 public class CalcTest {
   Calcmul cal = new Calcmul();
+  Calculator calc = new Calculator();
   
   @Test
   public void testSCalc1() {
     assertEquals("Result", 18, cal.mul());
+  }
+
+  @Test
+  public void testSCalc() {
+    assertEquals("Result", 9, calc.add());
   }
 }
